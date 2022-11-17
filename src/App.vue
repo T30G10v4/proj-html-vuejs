e8248175cf658143a0fc247cfa3bbcd135014154
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-11-17 12:39:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<template>
+
+  <h1>PROVA</h1>
+  <h2>PROVAS</h2>
+
+</template>
+
+<style lang="scss">
+
+@use "./styles/bss73.css" as *;
+@use "./styles/partials/variables" as *;
+@use "./styles/general.scss" as *;
+
+body {
+
+  background-color: $backgroundColor;
+  
+
+}
+
+</style>
